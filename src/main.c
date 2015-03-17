00b3635b060722d5c3b8728e1a50a5a1cfafe389
Julio Tain Sueiras <juliosueiras@gmail.com>
2015-03-16 20:50:08 -0400

    first commit for codes

---
+++ src/main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2015-03-16 08:36:23 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+int main(void)
+{
+
+    return 0;
+}
